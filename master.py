d010f470826af9edb524f177f2d17efe84b0e790
MetaIics <73257312+MetaIics@users.noreply.github.com>
2020-10-22 09:01:29 -0500

    Update master.py

--- master.py
+++ master.py
@@ -4,8 +4,10 @@ import os
 import json
 
 TOKEN = open("token.txt", "r").read()
-client = discord.Client()
+client = commands.Bot(command_prefix = 'm!')
 
+with open('cringe.json', 'r') as f:
+    crng = json.load(f)
 
 @client.event
 async def on_ready():
@@ -16,9 +18,30 @@ async def on_message(message):
     if message.author == client.user:
         return
 
-    if message.content.startswith("<:SillyChamp:743612208059252855>"):  
-        await message.channel.send("<:SillyChamp:743612208059252855>")
+    if message.content.startswith('<:SillyChamp:743612208059252855>'):  
+        await message.channel.send('<:SillyChamp:743612208059252855>')
         
-        
+    await client.process_commands(message)
     
+@client.command()
+async def poggers(ctx):
+    await ctx.send('<:PogU:742244756599603210>')
+    
+@client.command()
+async def sayname(ctx, user:discord.User):
+    await ctx.send(f"Your name is {user.mention}")
+
+@client.command()
+async def cringe(ctx, user:discord.User):
+    if not user.id in crng:
+        crng[user.id] = {}
+        crng[user.id]['count'] = 0
+        
+    crng[user.id]['count'] += 1
+    
+    await ctx.send(f"{user.mention} is cringe! Cringe counter now at {crng[user.id]['count']}.")
+    
+    with open('cringe.json','w') as f:
+        json.dump(crng,f)
+
 client.run(TOKEN)
